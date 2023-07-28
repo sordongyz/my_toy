14f44001b157a89aef482b557458be62df7e6413
sordongyz <62366933+sordongyz@users.noreply.github.com>
2023-07-28 17:13:23 +0800

    Toys: add kernel sys filesystem playing
    For this play, we create /sys/toy and create thress attrs:
    foo,bar,baz under the /sys/toy.
    The attrs can define its read/write method.
    The attrs interface can be used to instead origaql /proc interface
    Per Kernel Forum's point, proc interace is old and ugly.

--- my_toy.c
+++ my_toy.c
@@ -1,5 +1,6 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/device.h>
 #include <linux/kernel.h>
 #include <linux/io.h>
 #include <linux/delay.h>
@@ -18,7 +19,9 @@ static struct workqueue_struct *toy_wq = NULL;
 static struct work_struct toy_work;
 //static struct timer_list toy_timer;
 
-#define LED_6G_GREEN           0x6
+
+
+#define LED_6G_GREEN           0x15
 
 static void led_out(uint32_t val)
 {
@@ -36,12 +39,113 @@ static void blink_led(void)
 		led_out(0x0);
 		msleep(off_time_ms);
 	}
-	pr_emerg("blink led completed\n");
+	pr_info("blink led completed\n");
 
 }
 
-static int hello_init(void)
+
+/*
+ * This module shows how to create a simple subdirectory in sysfs called
+ * /sys/kernel/kobject-example  In that directory, 3 files are created:
+ * "foo", "baz", and "bar".  If an integer is written to these files, it can be
+ * later read out of it.
+ */
+
+static int foo;
+static int baz;
+static int bar;
+
+/*
+ * The "foo" file where a static variable is read from and written to.
+ */
+static ssize_t foo_show(struct kobject *kobj, struct kobj_attribute *attr,
+			char *buf)
 {
+	return sprintf(buf, "%d\n", foo);
+}
+
+static ssize_t foo_store(struct kobject *kobj, struct kobj_attribute *attr,
+			 const char *buf, size_t count)
+{
+	int ret;
+
+	ret = kstrtoint(buf, 10, &foo);
+	if (ret < 0)
+		return ret;
+
+	return count;
+}
+
+/* Sysfs attributes cannot be world-writable. */
+static struct kobj_attribute foo_attribute =
+	__ATTR(foo, 0664, foo_show, foo_store);
+
+/*
+ * More complex function where we determine which variable is being accessed by
+ * looking at the attribute for the "baz" and "bar" files.
+ */
+static ssize_t b_show(struct kobject *kobj, struct kobj_attribute *attr,
+		      char *buf)
+{
+	int var;
+
+	if (strcmp(attr->attr.name, "baz") == 0)
+		var = baz;
+	else
+		var = bar;
+	return sprintf(buf, "%d\n", var);
+}
+
+static ssize_t b_store(struct kobject *kobj, struct kobj_attribute *attr,
+		       const char *buf, size_t count)
+{
+	int var, ret;
+
+	ret = kstrtoint(buf, 10, &var);
+	if (ret < 0)
+		return ret;
+
+	if (strcmp(attr->attr.name, "baz") == 0)
+		baz = var;
+	else
+		bar = var;
+	return count;
+}
+
+static struct kobj_attribute baz_attribute =
+	__ATTR(baz, 0664, b_show, b_store);
+static struct kobj_attribute bar_attribute =
+	__ATTR(bar, 0664, b_show, b_store);
+
+
+/*
+ * Create a group of attributes so that we can create and destroy them all
+ * at once.
+ */
+static struct attribute *attrs[] = {
+	&foo_attribute.attr,
+	&baz_attribute.attr,
+	&bar_attribute.attr,
+	NULL,	/* need to NULL terminate the list of attributes */
+};
+
+/*
+ * An unnamed attribute group will put all of the attributes directly in
+ * the kobject directory.  If we specify a name, a subdirectory will be
+ * created for the attributes with the directory being the name of the
+ * attribute group.
+ */
+static struct attribute_group attr_group = {
+	.attrs = attrs,
+};
+
+static struct kobject *toy_kobj;
+
+
+static int toy_init(void)
+{
+	int result = 0;
+
 	gpiomem_base = ioremap(0x1000000,0x300000);
 	if (!gpiomem_base)
 		pr_emerg("Hello,this is a toy.led on %d ms,off %d ms,not mapped gpio registers\n",on_time_ms,off_time_ms);
@@ -52,19 +156,43 @@ static int hello_init(void)
 	toy_wq = alloc_workqueue("toy_wq", WQ_HIGHPRI | WQ_UNBOUND, 0);
 	INIT_WORK(&toy_work, (void *)blink_led);
 	queue_work(toy_wq,&toy_work);
-	return 0;
+
+
+	/*
+	 * Create a simple kobject with the name of "toy",
+	 * located under /sys/kernel/
+	 *
+	 * As this is a simple directory, no uevent will be sent to
+	 * userspace.  That is why this function should not be used for
+	 * any type of dynamic kobjects, where the name and number are
+	 * not known ahead of time.
+	 */
+	toy_kobj = kobject_create_and_add("toy",NULL);
+	//toy_kobj = kobject_create_and_add("toy", kernel_kobj);
+	if (!toy_kobj)
+		return -ENOMEM;
+
+	/* Create the files associated with this kobject */
+	result = sysfs_create_group(toy_kobj, &attr_group);
+	if (result)
+		kobject_put(toy_kobj);
+
+
+	return result;
 }
 
-static void hello_exit(void)
+static void toy_exit(void)
 {
 	if(gpiomem_base)
 		iounmap(gpiomem_base);
 
+	kobject_put(toy_kobj);
+
 	pr_emerg("Goobye,my toy\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(toy_init);
+module_exit(toy_exit);
 
 MODULE_AUTHOR("Yinzhi.Dong");
 MODULE_LICENSE("GPL");
